d659fef8d60829bef6871dadd0a0c05876fe76b6
Roman-Heviak <91373037+RomanHeviak@users.noreply.github.com>
2026-08-29 12:13:00 +0300

    ConnectionString

--- WebApplicationAPI/Program.cs
+++ WebApplicationAPI/Program.cs
@@ -51,6 +51,18 @@ builder.Services.AddScoped<IVideoGameCharacterService, VideoGameCharacterService
 builder.Services.AddScoped<IVideoGameService, VideoGameService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:5219",
+                "https://localhost:7247")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -61,6 +73,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 
